cc95cb3b39ec3695be9e5982b578542e07d0f7e0
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-04-12 20:40:57 +0200

    add formatter

--- init.lua
+++ init.lua
@@ -660,7 +660,15 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
       },
     },
   },
