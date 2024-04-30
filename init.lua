b3b8ab06f14a9537b82dfa7e4d889fe40ef7d599
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-04-30 11:39:29 +0200

    add dadbod-ui

--- init.lua
+++ init.lua
@@ -799,6 +799,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'vim-dadbod-completion' },
         },
       }
     end,
