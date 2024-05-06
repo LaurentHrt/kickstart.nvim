3cf1a0d8955cf319ee3ec21fca7f40878caaaa3a
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-05-06 19:16:59 +0200

    edit keymaps

--- init.lua
+++ init.lua
@@ -676,12 +676,12 @@ require('lazy').setup({
     lazy = false,
     keys = {
       {
-        '<leader>f',
+        '<leader>fb',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = '[F]ormat [B]uffer',
       },
     },
     opts = {
